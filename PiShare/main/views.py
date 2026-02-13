0df42f776872a1b49052a1016c1c882b4efc45ef
walidayankawsar <walidayankawsar@gmail.com>
2026-02-13 10:50:21 +0600

    session cleanup method setup

--- PiShare/main/views.py
+++ PiShare/main/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse, HttpResponse
+from .models import Session
+from .forms import FileForm
+import qrcode
+from django.conf import settings
+
 
 # Create your views here.
 
 
+
+
+def cleanup():
+    for s in Session.objects.all():
+        s.cleanup()   #model er cleanup method ke call korche
+
 def home(request):
+    cleanup()
     return render(request, 'index.html')
