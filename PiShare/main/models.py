0df31638a53282a4dd7268b1b789698ffa87ea09
walidayankawsar <walidayankawsar@gmail.com>
2026-02-13 11:02:21 +0600

    updated

--- PiShare/main/models.py
+++ PiShare/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
+from django.utils import timezone
+from datetime import timedelta
 
 # Create your models here.
+
+
+
+
+class Session(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    file = models.FileField(upload_to='files/', null=True, blank=True)
+    sender_role = models.CharField(max_length=20, default='laptop') #laptop, phone
+    status = models.CharField(max_length=20, default='waiting') #waiting, file_sent, expired
+
+    def is_expired(self):
+        return timezone.now()> self.created_at + timedelta(minutes=30)
+
+    def cleanup(self):
+        if self.is_expired():
+            if self.file:
+                self.file.delete(save=False)
+            self.delete()
