347f55c88a49954cf3fdf534d596b5dc877ce14c
walidayankawsar <walidayankawsar@gmail.com>
2026-02-13 21:11:45 +0600

    connect urls added

--- PiShare/main/urls.py
+++ PiShare/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('connect/<uuid:session_id>/', views.connect, name='connect')
 ]
