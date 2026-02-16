ce15fbd55a841ee9f0819eb5f46059b0f4bb4390
walidayankawsar <walidayankawsar@gmail.com>
2026-02-16 09:15:34 -0800

    url set for sent file

--- PiShare/main/urls.py
+++ PiShare/main/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('connect/<uuid:session_id>/', views.connect, name='connect')
+    path('connect/<uuid:session_id>/', views.connect, name='connect'),
+    path('sent_file/<uuid:session_id>/', views.send_from_phone, name='sent')
 ]
