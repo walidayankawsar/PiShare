091ae6cbc669f4ebc1be46161e8a1886479990eb
walidayankawsar <walidayankawsar@gmail.com>
2026-04-02 22:09:50 -0700

    receive urls added and updated

--- PiShare/main/urls.py
+++ PiShare/main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('connect/<uuid:session_id>/', views.connect, name='connect'),
-    path('sent_file/<uuid:session_id>/', views.send_from_phone, name='sent'),
-    path('check/<uuid:session_id>/', views.check_status, name='check')
+    path('sent_file/<uuid:session_id>/', views.send_from_phone, name='sent_file'),
+    path('receive-from-phone/<uuid:session_id>/', views.receive_from_phone, name='receive_from_phone'),
+    path('check_receive/<uuid:session_id>/', views.check_receive, name='check_receive')
 ]
