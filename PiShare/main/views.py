8feb71f7eb074daac125ea0039ebfeaa10c4ca8a
walidayankawsar <walidayankawsar@gmail.com>
2026-04-10 22:44:07 +0600

    just fixed bugs

--- PiShare/main/views.py
+++ PiShare/main/views.py
@@ -11,23 +11,23 @@ import base64
 # Create your views here.
 
 
-
-
 def cleanup():
     for s in Session.objects.all():
-        s.cleanup()   #model er cleanup method ke call korche
+        s.cleanup()  # model er cleanup method ke call korche
+
 
 def home(request):
     cleanup()
     session = Session.objects.create(sender_role='Laptop or Desktop')
-    qr_url = request.build_absolute_uri(f'/connect/{session.id}/') #url make * aita connect page er url so connect views and url make korte hobe.
+    # url make * aita connect page er url so connect views and url make korte hobe.
+    qr_url = request.build_absolute_uri(f'/connect/{session.id}/')
     qr = qrcode.QRCode(version=1, box_size=10, border=4)
     qr.add_data(qr_url)
     qr.make(fit=True)
     img = qr.make_image(fill_color='black', back_color='white')
-                                                                    # QR image ke HTML page e show korar jonno text format e convert kora.Because browser directly Python image object bujhe na.
+    # QR image ke HTML page e show korar jonno text format e convert kora.Because browser directly Python image object bujhe na.
 
-    buffer = BytesIO() # BytesIO() holo ekta temporary memory file.
+    buffer = BytesIO()  # BytesIO() holo ekta temporary memory file.
     img.save(buffer, format='PNG')
     qr_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
@@ -35,7 +35,6 @@ def home(request):
         'qr_code': qr_base64,
         'session_id': str(session.id)
     })
-
 
 
 def connect(request, session_id):
@@ -48,14 +47,13 @@ def connect(request, session_id):
     })
 
 
-
 def send_from_phone(request, session_id):
     cleanup()
     session = get_object_or_404(Session, id=session_id)
 
     if session.is_expired():
         return HttpResponse("Session expired", status=410)
-    
+
     if request.method == "POST":
         form = FileForm(request.POST, request.FILES)
         if form.is_valid():
@@ -71,24 +69,24 @@ def send_from_phone(request, session_id):
 def receive_from_phone(request, session_id):
     cleanup()
     session = get_object_or_404(Session, id=session_id)
+
     if session.is_expired():
-        return JsonResponse({'status': 'expaired'})
-    
-    session.status = 'file_sent'
-    session.save()
-    return render(request, 'receive.html', {'session_id' :session_id})
+        return JsonResponse({'status': 'expired'})
+
+    # POST request মানে button click হয়েছে
+    if request.method == 'POST':
+        session.status = 'file_sent'
+        session.save()
+        return JsonResponse({'status': 'ok'})
+
+    return render(request, 'receive.html', {'session_id': session_id})
 
 
 def check_receive(request, session_id):
     cleanup()
     session = get_object_or_404(Session, id=session_id)
+
     if session.is_expired():
-        return HttpResponse('Session expired', status= 410)
-    
-    if session.status == 'file_sent':
-        return render(request, 'sand_file.html', {'session_id': session_id})
-    
-    return render(request, 'index.html', {
-        'qr_code': None,
-        'session_id': str(session.id)
-    })
+        return JsonResponse({'status': 'expired'})
+
+    return JsonResponse({'status': session.status})
