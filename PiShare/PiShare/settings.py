8700377b2f2c5afb23b4e38a2d18a3e84669b411
walidayankawsar <walidayankawsar@gmail.com>
2026-04-10 22:12:18 +0600

    media root and url added

--- PiShare/PiShare/settings.py
+++ PiShare/PiShare/settings.py
@@ -118,3 +118,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
