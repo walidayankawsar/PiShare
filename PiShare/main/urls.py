55c1263dad15865b3cbc4030eefa65cb48535981
walidayankawsar <walidayankawsar@gmail.com>
2026-04-14 12:42:05 +0600

    receive and send file url added

--- PiShare/main/urls.py
+++ PiShare/main/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('connect/<uuid:session_id>/', views.connect, name='connect'),
+    path('send_file/<uuid:session_id>/', views.send_file, name='send_file'),
+    path('receive_file/<uuid:session_id>/', views.receive_file, name='receive_file'),
 ]
 
 
