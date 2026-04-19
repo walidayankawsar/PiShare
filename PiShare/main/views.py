fcf6b53cd52f40f8595c1bc40d95299141391034
walidayankawsar <walidayankawsar@gmail.com>
2026-04-19 12:01:05 +0600

    bug fixed - require_http_methods import

--- PiShare/main/views.py
+++ PiShare/main/views.py
@@ -5,6 +5,7 @@ from .forms import FileForm
 import qrcode
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 from io import BytesIO
 import base64
