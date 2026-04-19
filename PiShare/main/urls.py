d3b7ea06bd89df37b87cffcc9bcea0076a222d2d
walidayankawsar <walidayankawsar@gmail.com>
2026-04-20 02:45:21 +0600

    fixed bux and updated

--- PiShare/main/urls.py
+++ PiShare/main/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path('connect/<uuid:session_id>/', views.connect, name='connect'),
     path('send_file/<uuid:session_id>/', views.send_file, name='send_file'),
     path('receive_file/<uuid:session_id>/', views.receive_file, name='receive_file'),
+
+
+    path('api/upload/<uuid:session_id>/', views.upload_file, name='upload_file'),
+    path('api/download/<uuid:session_id>/<uuid:file_id>/', views.download_file, name='download_file'),
+    path('api/download-all/<uuid:session_id>/', views.download_all_files, name='download_all'),
 ]
 
 
